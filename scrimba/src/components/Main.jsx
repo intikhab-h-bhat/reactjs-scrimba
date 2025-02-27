9826e37210270984a6f84923dfa9f6206b693144
intikhab-h-bhat <intikhabh@gmail.com>
2025-02-27 12:52:16 +0530

    first  proj completed

---
+++ scrimba/src/components/Main.jsx
@@ -0,0 +1,15 @@
+export default function Main(){
+
+    return (
+        <main>
+        <h1 className="heading">Fun Facts About React</h1>
+        <ul className="list-items">
+            <li>Was first relased in 2002</li>
+            <li>Was orgionally created by</li>
+            <li>Available in GitHub</li>
+            <li>Is Maintained by meta</li>
+        </ul>
+        </main>
+    )
+
+}
---
+++ scrimba/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar(){
+
+    return(
+
+        <header className="header">
+            <nav className="navbar">
+                <img className="nav-image" src="./src/images/Vector.png"  />
+                <h3>ReactFacts</h3>
+            </nav>
+
+        </header>
+        
+    )
+}
